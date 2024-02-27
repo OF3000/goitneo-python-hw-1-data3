4d563eac203afe65d62ac200930fd40a5d6a4cb6
OF3000 <ofpost@i.ua>
2024-02-27 14:59:28 +0200

    init commit

--- 1.py
+++ 1.py
@@ -1,56 +1,57 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from collections import defaultdict
 
 
-users=[{"name": "Bill Gates", "birthday": datetime(1955, 1, 28)},
-       {"name": "Jimm Gates", "birthday": datetime(1955, 2, 28)},
-       {"name": "John Gates", "birthday": datetime(1955, 2, 25)},
-       {"name": "Tom Gates", "birthday": datetime(1955, 2, 26)},
-       {"name": "Ben Gates", "birthday": datetime(1955, 3, 2)},
-       {"name": "Peter Gates", "birthday": datetime(1955, 3, 3)}
-       ]
+users = [
+    {"name": "Bill Gates", "birthday": datetime(1955, 1, 28)},
+    {"name": "Jimm Gates", "birthday": datetime(1955, 2, 28)},
+    {"name": "John Gates", "birthday": datetime(1955, 2, 25)},
+    {"name": "Tom Gates", "birthday": datetime(1955, 2, 26)},
+    {"name": "Ben Gates", "birthday": datetime(1955, 3, 2)},
+    {"name": "Peter Gates", "birthday": datetime(1955, 3, 3)},
+]
+
 
 def get_birthdays_per_week(users):
-    today=datetime.today().date()
-    #today=datetime(2024,2,26).date()
-    bd=defaultdict(list)
+    today = datetime.today().date()
+    # today=datetime(2024,2,26).date()
+    bd = defaultdict(list)
 
-    tw=today.weekday()
-    #print(tw)
+    tw = today.weekday()
+    # print(tw)
     print(today)
     for user in users:
         name = user["name"]
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
         if birthday_this_year < today:
-            birthday_this_year.replace(year=today.year+1)
+            birthday_this_year.replace(year=today.year + 1)
         delta_days = (birthday_this_year - today).days
-        if tw-2 <= delta_days < 7 : 
-            #print(birthday_this_year)
-            bw=birthday_this_year.strftime('%A')
-            if bw in ('Saturday','Sunday') and birthday_this_year<today:
-                bw='Monday'
-            elif bw in ('Saturday','Sunday') and birthday_this_year>today:
+        if tw - 2 <= delta_days < 7:
+            # print(birthday_this_year)
+            bw = birthday_this_year.strftime("%A")
+            if bw in ("Saturday", "Sunday") and birthday_this_year < today:
+                bw = "Monday"
+            elif bw in ("Saturday", "Sunday") and birthday_this_year > today:
                 continue
-            elif birthday_this_year<today:
+            elif birthday_this_year < today:
                 continue
             bd[bw].append(name)
-            #bd[bw]=name
-            #bu.append(bd.copy())
-            
-    #print(bd)
-    if len(bd)>0:
+            # bd[bw]=name
+            # bu.append(bd.copy())
+
+    # print(bd)
+    if len(bd) > 0:
         for i in range(7):
-            tw=(today+timedelta(days=i)).strftime('%A')
+            tw = (today + timedelta(days=i)).strftime("%A")
             if not bd[tw]:
                 continue
-            s=', '.join(bd[tw])
-            print(f'{tw}: {s}')
-    else: print ('No more Birthdays this week')
+            s = ", ".join(bd[tw])
+            print(f"{tw}: {s}")
+    else:
+        print("No more Birthdays this week")
 
-
-
-    #print(users)
+    # print(users)
 
 
 get_birthdays_per_week(users)

bbfbc2a6de84189f37d0a780c88a7b2bb659859e
OF3000 <ofpost@i.ua>
2024-02-27 16:15:47 +0200

    final commit

--- 2.py
+++ 2.py
@@ -3,24 +3,29 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
+
 
 def upd_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact changed."
 
+
 def get_contact(args, contacts):
-    name = ''.join(args)
+    name = "".join(args)
     phone = contacts[name]
     return f"Phone is: {phone}"
 
+
 def all_contact(contacts):
-    for u,p in contacts.items():
-        print(f'{u} {p}')
+    for u, p in contacts.items():
+        print(f"{u} {p}")
+
 
 def main():
     contacts = {}
@@ -28,7 +33,7 @@ def main():
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-        
+
         if command in ["close", "exit"]:
             print("Good bye!")
             break
@@ -44,6 +49,7 @@ def main():
             print(all_contact(contacts))
         else:
             print("Invalid command.")
+
 
 if __name__ == "__main__":
     main()
